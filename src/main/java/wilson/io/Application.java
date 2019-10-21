50a1e9f6d0ca37f24d00e86329ef8fd35a46d2b7
wilson <39546525@qq.com>
2019-10-22 00:33:52 +0800

    1st

---
+++ src/main/java/wilson/io/Application.java
@@ -0,0 +1,14 @@
+package wilson.io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by wilson on 2019/5/11.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
